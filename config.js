ce50f5665fff5590e7fdfc6b05604e62f1904fe4
AnyThony <anthonynguyenstudent@gmail.com>
2020-05-31 04:30:23 -0400

    config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "tenorKey": "API_KEY_HERE",
+    "contentLimit": 30,
+    "contentKeyword": "doggo",
+    "enableFakeVotes": true,
+    "mongoConnection": 'DB_STRING_HERE'
+}
